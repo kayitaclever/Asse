5223959eeb6bed3c81ecd22c90a5d691943af81b
kayitaclever <kayitaramirwaaclever@gmail.com>
2023-03-09 08:58:09 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+import kotlin.math.max
+
+fun main(){
+  students("Clever","Linda")
+    var x= "clever"[0]
+    println(x)
+    Numbers(34,23,15)
+    println(maxOf(34,23,15))
+    println(minOf(34,23,15))
+    book("Born a crime ")
+    println()
+     females("marie","Joy",)
+    println()
+    var calculations = calculator(2,5,6,3)
+    println(calulation.)
+
+}
+
+
+
+fun students(name1:String,name2:String):String{
+    var character = name1[0]
+    return character
+}
+
+fun  Numbers(num1:Int,num2:Int,num3:Int):Int{
+    var numbers= arrayOf(num1,num2,num3)
+
+    return numbers
+
+}
+  fun book(name:String){
+
+  }
+
+fun females (nam1:String,name2:String):String{
+
+    return females
+}
+
+class calculator (var num1 :Int,var num2:Int, var num3:Int,var num4:Int)
+      fun addition(){
+      }
+
